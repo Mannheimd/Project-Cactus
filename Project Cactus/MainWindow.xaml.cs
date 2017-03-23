dbff3d869fbaa382d447d7bbfd3737b4083ef421
Chris Manders <cmanders159@gmail.com>
2017-03-23 16:18:03 +0000

    UI building
    - Added Environmental Info section
    - Added Issue Information section

--- Project Cactus/MainWindow.xaml.cs
+++ Project Cactus/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -28,6 +30,60 @@ namespace Project_Cactus
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Clipboard.SetText("Hello!");
+        }
+
+        private void actVersionNumber_OtherSelected(object sender, RoutedEventArgs e)
+        {
+            actVersion_TextBox.IsEnabled = true;
+        }
+
+        private void actVersionNumber_NotOtherSelected(object sender, RoutedEventArgs e)
+        {
+            actVersion_TextBox.IsEnabled = false;
+        }
+
+        private void officeVersion_OtherSelected(object sender, RoutedEventArgs e)
+        {
+            officeVersion_TextBox.IsEnabled = true;
+        }
+
+        private void officeVersion_NotOtherSelected(object sender, RoutedEventArgs e)
+        {
+            officeVersion_TextBox.IsEnabled = false;
+        }
+
+        private void windowsVersion_OtherSelected(object sender, RoutedEventArgs e)
+        {
+            windowsVersion_TextBox.IsEnabled = true;
+        }
+
+        private void windowsVersion_NotOtherSelected(object sender, RoutedEventArgs e)
+        {
+            windowsVersion_TextBox.IsEnabled = false;
+        }
+
+        private void reasonForCallInformation_Image_MouseCheck(object sender, MouseEventArgs e)
+        {
+            if (reasonForCallInformation_Image.IsMouseOver)
+            {
+                reasonForCallInformation_Text.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                reasonForCallInformation_Text.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void errorMessagesInformation_Image_MouseCheck(object sender, MouseEventArgs e)
+        {
+            if (errorMessagesInformation_Image.IsMouseOver)
+            {
+                errorMessagesInformation_Text.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                errorMessagesInformation_Text.Visibility = Visibility.Hidden;
+            }
         }
     }
 }
