6ba2cdff4b236937a4430bf61914bc5dc53121af
Chris Manders <cmanders159@gmail.com>
2017-03-28 20:52:20 +0100

    Completed timer, implemented reset

--- Project Cactus/MainWindow.xaml.cs
+++ Project Cactus/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Project_Cactus
 {
@@ -23,71 +24,80 @@ namespace Project_Cactus
         // Values used for logging KCS things
         public string productFamily;
 
-        // Timestamp for working out call duration
+        // Values for working out call duration
         public DateTime startTime;
+        public DateTime endTime;
+        public bool isTimerRunning = false;
         public TimeSpan callDuration;
+        public TimeSpan finalCallDuration;
+
+        // Timer for on-screen display of call duration
+        DispatcherTimer durationCounter = new DispatcherTimer();
 
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void copyToClipboard_Button_Click(object sender, RoutedEventArgs e)
-        {
-            Clipboard.SetText("Hello!");
-        }
-
         private void productComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             // Populates the Version drop-down with items depending on what product was selected, and makes the Version drop-down visible. Some products don't require the Version drop-down to appear.
             // Also sets the "Product Family" and "Integration" strings to fill in the KCS stuff.
-            string selectedItem = (e.AddedItems[0] as ComboBoxItem).Content as string;
-            switch (selectedItem)
+            if (e.AddedItems.Count > 0)
             {
-                case "Act! Pro":
-                    setVersionDropDown(true, false, "actVersions");
-                    productFamily = "Act!";
-                    break;
+                string selectedItem = (e.AddedItems[0] as ComboBoxItem).Content as string;
+                switch (selectedItem)
+                {
+                    case "Act! Pro":
+                        setVersionDropDown(true, false, "actVersions");
+                        productFamily = "Act!";
+                        break;
 
-                case "Act! Premium":
-                    setVersionDropDown(true, false, "actVersions");
-                    productFamily = "Act!";
-                    break;
+                    case "Act! Premium":
+                        setVersionDropDown(true, false, "actVersions");
+                        productFamily = "Act!";
+                        break;
 
-                case "Act! Premium for Web":
-                    setVersionDropDown(true, false, "actVersions");
-                    productFamily = "Act!";
-                    break;
+                    case "Act! Premium for Web":
+                        setVersionDropDown(true, false, "actVersions");
+                        productFamily = "Act!";
+                        break;
 
-                case "Act! Premium Cloud":
-                    setVersionDropDown(false, false, "blankList");
-                    productFamily = "Act!";
-                    break;
+                    case "Act! Premium Cloud":
+                        setVersionDropDown(false, false, "blankList");
+                        productFamily = "Act!";
+                        break;
 
-                case "Act! Emarketing":
-                    setVersionDropDown(true, false, "actVersions");
-                    productFamily = "Act!";
-                    break;
+                    case "Act! Emarketing":
+                        setVersionDropDown(true, false, "actVersions");
+                        productFamily = "Act!";
+                        break;
 
-                case "Swiftpage Emarketing":
-                    setVersionDropDown(false, false, "blankList");
-                    productFamily = "Emarketing";
-                    break;
+                    case "Swiftpage Emarketing":
+                        setVersionDropDown(false, false, "blankList");
+                        productFamily = "Emarketing";
+                        break;
 
-                case "Other":
-                    setVersionDropDown(false, false, "blankList");
-                    productFamily = "Other";
-                    break;
+                    case "Other":
+                        setVersionDropDown(false, false, "blankList");
+                        productFamily = "Other";
+                        break;
 
-                case "N/A":
-                    setVersionDropDown(false, false, "blankList");
-                    productFamily = "N/A";
-                    break;
+                    case "N/A":
+                        setVersionDropDown(false, false, "blankList");
+                        productFamily = "N/A";
+                        break;
 
-                default:
-                    setVersionDropDown(false, false, "blankList");
-                    productFamily = null;
-                    break;
+                    default:
+                        setVersionDropDown(false, false, "blankList");
+                        productFamily = null;
+                        break;
+                }
+            }
+            else
+            {
+                setVersionDropDown(false, false, "blankList");
+                productFamily = null;
             }
         }
 
@@ -112,18 +122,130 @@ namespace Project_Cactus
 
         private void startTimerButton_Click(object sender, RoutedEventArgs e)
         {
-            endTimerButton_Button.IsEnabled = true;
-            startTimerButton_Button.IsEnabled = false;
+            // Enable/disable buttons
+            endTimer_Button.IsEnabled = true;
+            startTimer_Button.IsEnabled = false;
+            resetForm_Button.IsEnabled = false;
+            copyToClipboard_Button.IsEnabled = false;
+
+            // Save current time and call duration, start counting new duration
             startTime = DateTime.Now;
+            finalCallDuration = finalCallDuration + callDuration;
+            callDuration = new TimeSpan();
+            isTimerRunning = true;
+
+            // Start on-screen counting timer
+            durationCounter.Tick += new EventHandler(durationCounter_Tick);
+            durationCounter.Interval = new TimeSpan(0, 0, 1);
+            durationCounter.Start();
         }
 
         private void endTimerButton_Click(object sender, RoutedEventArgs e)
         {
-            endTimerButton_Button.IsEnabled = false;
-            startTimerButton_Button.IsEnabled = true;
-            callDuration = callDuration + (DateTime.Now - startTime);
+            // Enable/disable buttons
+            endTimer_Button.IsEnabled = false;
+            startTimer_Button.IsEnabled = true;
+            resetForm_Button.IsEnabled = true;
+            copyToClipboard_Button.IsEnabled = true;
 
-            MessageBox.Show(callDuration.Minutes.ToString());
+            // Save final call duration (resets when reset button is pressed)
+            endTime = DateTime.Now;
+            calculateCallDuration();
+            isTimerRunning = false;
+
+            // End on-screen counting timer
+            durationCounter.Stop();
+        }
+
+        private void resetFormButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Reset timer
+            callDuration = new TimeSpan();
+            finalCallDuration = new TimeSpan();
+            callDuration_Label.Content = "00:00:00";
+
+            // Reset all fields and drop-downs
+            reasonForCall_TextBox.Text = null;
+            product_ComboBox.SelectedIndex = -1;
+            environmentOs_ComboBox.SelectedIndex = -1;
+            environmentBrowser_ComboBox.SelectedIndex = -1;
+            environmentSql_ComboBox.SelectedIndex = -1;
+            environmentOffice_ComboBox.SelectedIndex = -1;
+            environmentOther_TextBox.Text = null;
+            errorMessages_TextBox.Text = null;
+            stepsTaken_TextBox.Text = null;
+            additionalInformation_TextBox.Text = null;
+            resolution_TextBox.Text = null;
+        }
+
+        private void copyToClipboard_Button_Click(object sender, RoutedEventArgs e)
+        {
+            string outputString = String.Format(@"Environmental Info:
+- Act version: {0} {1}
+- Windows version: {2}
+- Office version: {3}
+- Web browser: {4}
+- SQL: {5}
+- Other: {6}
+
+---
+Reason for Call:
+{7}
+
+---
+Error messages:
+{8}
+
+---
+Additional issue/query information:
+{9}
+
+---
+Steps Taken:
+{10}
+
+---
+Solution:
+{11}
+
+---
+Duration: {12}",
+            product_ComboBox.Text,
+            productVersion_ComboBox.Text,
+            environmentOs_ComboBox.Text,
+            environmentOffice_ComboBox.Text,
+            environmentBrowser_ComboBox.Text,
+            environmentSql_ComboBox.Text,
+            environmentOther_TextBox.Text,
+            reasonForCall_TextBox.Text,
+            errorMessages_TextBox.Text,
+            additionalInformation_TextBox.Text,
+            stepsTaken_TextBox.Text,
+            resolution_TextBox.Text,
+            calculateCallDuration());
+
+            Clipboard.SetText(outputString);
+        }
+
+        private void durationCounter_Tick(object sender, EventArgs e)
+        {
+            // Get current call duration, 
+            callDuration_Label.Content = calculateCallDuration();
+        }
+
+        private string calculateCallDuration()
+        {
+            // Set current call duration, and also return total call duration as a string
+            if (isTimerRunning)
+            {
+                callDuration = DateTime.Now - startTime;
+            }
+            else
+            {
+                callDuration = endTime - startTime;
+            }
+            TimeSpan tempCallDuration = finalCallDuration + callDuration;
+            return tempCallDuration.ToString().Substring(0, 8);
         }
     }
 
